fdb1359feab1f75cbc21ea31fda2b0cabb110f39
liwenqian0520 <50043720+liwenqian0520@users.noreply.github.com>
2019-11-25 18:48:14 +0800

    建包

---
+++ xiangmu/src/main/java/com/example/demo/rep/MechanismRep.java
@@ -0,0 +1,9 @@
+package com.example.demo.rep;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.entity.Mechanism;
+
+public interface MechanismRep extends JpaRepository<Mechanism, Integer>{
+
+}
